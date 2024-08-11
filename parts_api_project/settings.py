fc47d7038638e415db5696f958fe10e2515c825d
Alexseyka07 <alex07progtime@gmail.com>
2024-08-11 13:17:56 +0300

    add_env

--- parts_api_project/settings.py
+++ parts_api_project/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = "parts_api_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "76324338",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
